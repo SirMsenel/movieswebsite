38490998092e565274f99c84b91f5ad3999b4a11
mehmet şenel <senelm263@gmail.com>
2024-05-02 14:05:26 +0300

    class and admin

--- movies/views.py
+++ movies/views.py
@@ -1,54 +1,23 @@
 from django.shortcuts import render
-
-kategori_liste = ["macera","romantik","dram","Bilim kurgu"]
-film_liste = [
-    {
-        "id" : 1,
-        "film_adi" : "film 1",
-        "aciklama" : "film 1 açıklama",
-        "resim" : "1.jpg",
-        "anasayfa" : True
-    },
-    {
-        "id" : 2,
-        "film_adi" : "film 2",
-        "aciklama" : "film 2 açıklama",
-        "resim" : "2.jpg",
-        "anasayfa" : True
-    },
-    {
-        "id" : 3,
-        "film_adi" : "film 3",
-        "aciklama" : "film 3 açıklama",
-        "resim" : "3.jpg",
-        "anasayfa" : True
-    },
-    {
-        "id" : 4,
-        "film_adi" : "film 4",
-        "aciklama" : "film 4 açıklama",
-        "resim" : "4.jpg",
-        "anasayfa" : False
-    }
-]
+from .models import Category,Movie
 
 def home(request):
     data = {
-        "kategoriler" : kategori_liste,
-        "filmler" : film_liste
+        "kategoriler" : Category.objects.all(),
+        "filmler" : Movie.objects.filter(anasayfa=True)
     }
     return render(request,"index.html", data)
 
 def movies(request):
     data = {
-        "kategoriler" : kategori_liste,
-        "filmler" : film_liste
+        "kategoriler" : Category.objects.all(),
+        "filmler" : Movie.objects.all
     }
     return render(request,"movies.html",data)
 
 
 def moviedetails(request, id):
     data = {
-       "id" :id 
+       "movie" : Movie.objects.get(id=id) 
     }
     return render(request,"details.html",data)
